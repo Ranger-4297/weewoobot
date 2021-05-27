b5144f62b7c3c0283d7afa455b069727d70de0c2
Ranger-4297 <discdynamic@gmail.comom>
2021-05-27 22:03:03 +0100

    Status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,23 +1,4 @@
 module.exports = async (client) => {
-  
-  const activities = [
-    { name: 'Master', type: 'LISTENING' }, 
-    { name: 'Daddy rangy', type: 'LISTENING' }
-  ];
-
-  // Update presence
-  client.user.setPresence({ status: 'online', activity: activities[0] });
-
-  let activity = 1;
-
-  // Update activity every 30 seconds
-  setInterval(() => {
-    activities[2] = { name: `Ranger jack off`, type: 'WATCHING' }; // Update server count
-    activities[3] = { name: `Ranger moan`, type: 'LISTENING' }; // Update user count
-    if (activity > 3) activity = 0;
-    client.user.setActivity(activities[activity]);
-    activity++;
-  }, 30000);
 
   client.logger.info('Updating database and scheduling jobs...');
   for (const guild of client.guilds.cache.values()) {
