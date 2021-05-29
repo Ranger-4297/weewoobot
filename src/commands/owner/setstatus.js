711749e3f5b7afe990e850a5d030b95fb3bf2dec
Ranger-4297 <discdynamic@gmail.comom>
2021-05-29 01:40:37 +0100

    fix

--- src/commands/owner/setstatus.js
+++ src/commands/owner/setstatus.js
@@ -6,7 +6,7 @@ module.exports  = class Setstatus extends Command {
         name: "setstatus",
         aliases: ['setsat', 'status', 'set-status'],
         usage: 'setStatus <Activity: LISTENING|WATCHING|PLAYING> <Status: String>',
-        description: "Sets the bot status & activity",
+        description: "Sets the bots status & activity",
         type: client.types.OWNER,
         ownerOnly: true,
     });
