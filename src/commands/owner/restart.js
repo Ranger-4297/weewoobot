9ba40467b09c03274a3e33a2de75fe02f1781521
Ranger-4297 <discdynamic@gmail.comom>
2021-05-31 02:34:20 +0100

    (Update)Fix

--- src/commands/owner/restart.js
+++ src/commands/owner/restart.js
@@ -1,23 +1,23 @@
 const Command = require('../Command.js');
 const { MessageEmbed } = require('discord.js');
 
-module.exports  = class restart extends Command {
+module.exports  = class killbot extends Command {
   constructor(client) {
     super(client, {
-        name: "restart",
-        usage: 'restart',
-        description: "Restarts the bot\nRequires PM2 to reload",
+        name: "killbot",
+        usage: 'killbot',
+        description: "Kills the bot",
         type: client.types.OWNER,
         ownerOnly: true,
     });
   }
 async run(message) {
   const embed = new MessageEmbed()
-  .setDescription('Successfully reloaded the bot...give it some time')
+  .setDescription('Successfully terminated the instance of the bot...give it some time')
   .setTimestamp()
   .setColor('#2f3136');
 message.channel.send(embed);
-console.log('Bot was reloaded')
+console.log('Bot was killed')
 setTimeout(function(){ 
     process.exit()
  }, 5000); //time in milliseconds
