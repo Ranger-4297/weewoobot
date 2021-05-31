1616d53fb32644ba76bee3b0dddd1a4e33a76968
Ranger <72880161+Ranger-4297@users.noreply.github.com>
2021-05-31 02:39:11 +0100

    (Restart)

--- src/commands/owner/restart.js
+++ src/commands/owner/restart.js
@@ -4,20 +4,20 @@ const { MessageEmbed } = require('discord.js');
 module.exports  = class killbot extends Command {
   constructor(client) {
     super(client, {
-        name: "killbot",
-        usage: 'killbot',
-        description: "Kills the bot",
+        name: "restart",
+        usage: 'restart',
+        description: "restarts the bot",
         type: client.types.OWNER,
         ownerOnly: true,
     });
   }
 async run(message) {
   const embed = new MessageEmbed()
-  .setDescription('Successfully terminated the instance of the bot...give it some time')
+  .setDescription('Bot restarting now...give it some time')
   .setTimestamp()
   .setColor('#2f3136');
 message.channel.send(embed);
-console.log('Bot was killed')
+console.log('Bot was restarted')
 setTimeout(function(){ 
     process.exit()
  }, 5000); //time in milliseconds
