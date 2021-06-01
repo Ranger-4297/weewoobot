fc621ebc92599f112ff742723fe7369f3d841211
Ranger-4297 <discdynamic@gmail.comom>
2021-06-01 07:11:39 +0100

    (Update)Allows forr activity type

--- src/commands/owner/setstatus.js
+++ src/commands/owner/setstatus.js
@@ -5,8 +5,8 @@ module.exports  = class Setstatus extends Command {
     super(client, {
         name: "setstatus",
         aliases: ['setsat', 'status', 'set-status'],
-        usage: 'setStatus <status>',
-        description: "Sets the bots status",
+        usage: 'setStatus <activity> <status>',
+        description: "Sets the bots presence. Valid activities are: LISTENING|WATCHING|PLAYING",
         type: client.types.OWNER,
         ownerOnly: true,
     });
@@ -14,12 +14,14 @@ module.exports  = class Setstatus extends Command {
   async run(message, args) {
     //ARGUMENT
     if(args[0]) {
-      this.client.user.setPresence({ activity: { name: (args.join(" ")) , type: 'WATCHING' }, status: 'online'})
+      this.client.user.setPresence({ activity: { type: (args[0]), name: (args.slice(1).join(' ')), }, status: 'online'})
       message.channel.send("Updated the bot status")
-      console.log(`Status updated: ${(args.join(" "))}`)
+      // to do: Embed the above & below error
+      console.log(`Presence updated: Activity: ${(args[0])}, Status: ${(args.slice(1).join(' '))}`)
       }
     else if(args) {
-      return this.sendErrorMessage(message, 0, ("Please give a status message"))
+      return this.sendErrorMessage(message, 0, ("Please give a valid activity."))
+      return this.sendErrorMessage(message, 1, ("Please enter a status"))
     }
   }
 };
