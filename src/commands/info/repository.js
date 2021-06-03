8e10117f5ee01fc079df1f04809034e31bb564ec
Ranger-4297 <discdynamic@gmail.comom>
2021-06-03 18:02:58 +0100

    (Update)Name change and cleanup

--- src/commands/info/repository.js
+++ src/commands/info/repository.js
@@ -1,32 +1,28 @@
 const Command = require('../Command.js');
 const { MessageEmbed } = require('discord.js');
-const { oneLine } = require('common-tags');
-const { MessageButton } = require('discord-buttons');
+const { MessageButton } = require('discord-buttons')
 
 module.exports = class GitHubCommand extends Command {
   constructor(client) {
     super(client, {
-      name: 'github',
-      aliases: ['gh', 'repo'],
-      usage: 'github',
-      description: 'Displays the link to the owners\'s GitHub account.',
+      name: 'repository',
+      aliases: ['repo'],
+      usage: 'repository',
+      description: 'Displays the link to the bot\'s GitHub repository.',
       type: client.types.INFO
     });
   }
   run(message) {
     const embed1 = new MessageEmbed()
-      .setTitle('GitHub Link')
-      .setDescription(oneLine`
-        Please support me by starring & following me, feel free to contribute to any projects I own, run and contribute to!
-      `)
+      .setTitle('Repository Link')
+      .setDescription("Please support me by starring & following me, feel free to contribute to any projects I own, run and contribute to!")
       .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
       .setTimestamp()
       .setColor(message.guild.me.displayHexColor);
     let button = new MessageButton()
-      .setLabel("GitHub")
+      .setLabel("Repository")
       .setStyle("url")
-      .setURL("https://github.com/Ranger-4297")
-
+      .setURL("https://github.com/Ranger-4297/Weewoo")
       message.channel.send ({ embed: embed1, button })
     }
 };
