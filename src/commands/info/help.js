5701ac5ede4f922a709441f422d9a581ebaadcc0
Ranger-4297 <discdynamic@gmail.comom>
2021-06-03 18:03:37 +0100

    (Update)Cleanup

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -2,7 +2,6 @@ const Command = require('../Command.js');
 const { MessageEmbed } = require('discord.js');
 const emojis = require('../../utils/emojis.json');
 const { oneLine, stripIndent } = require('common-tags');
-const { MessageButton, MessageActionRow } = require('discord-buttons');
 const disbut = require('discord-buttons')
 
 module.exports = class HelpCommand extends Command {
@@ -21,17 +20,14 @@ module.exports = class HelpCommand extends Command {
     });
   }
   run(message, args) {
-
     // Get disabled commands
     let disabledCommands = message.client.db.settings.selectDisabledCommands.pluck().get(message.guild.id) || [];
     if (typeof(disabledCommands) === 'string') disabledCommands = disabledCommands.split(' ');
-
     const all = (args[0] === 'all') ? args[0] : '';
     const embed = new MessageEmbed();
     const prefix = message.client.db.settings.selectPrefix.pluck().get(message.guild.id); // Get prefix
     const { INFO, FUN, COLOR, POINTS, MISC, MOD, ADMIN, OWNER } = message.client.types;
     const { capitalize } = message.client.utils;
-    
     const command = message.client.commands.get(args[0]) || message.client.aliases.get(args[0]);
     if (
       command && 
@@ -49,19 +45,15 @@ module.exports = class HelpCommand extends Command {
         .setColor(message.guild.me.displayHexColor);
       if (command.aliases) embed.addField('Aliases', command.aliases.map(c => `\`${c}\``).join(' '));
       if (command.examples) embed.addField('Examples', command.examples.map(c => `\`${prefix}${c}\``).join('\n'));
-
       message.channel.send(embed)
-
     } else if (args.length > 0 && !all) {
       return this.sendErrorMessage(message, 0, 'Unable to find command, please check provided command');
     } else {
-
       // Get commands
       const commands = {};
       for (const type of Object.values(message.client.types)) {
         commands[type] = [];
       }
-
       const emojiMap = {
         [INFO]: `${emojis.info} ${capitalize(INFO)}`,
         [FUN]: `${emojis.fun} ${capitalize(FUN)}`,
@@ -72,7 +64,6 @@ module.exports = class HelpCommand extends Command {
         [ADMIN]: `${emojis.admin} ${capitalize(ADMIN)}`,
         [OWNER]: `${emojis.owner} ${capitalize(OWNER)}`
       };
-
       message.client.commands.forEach(command => {
         if (!disabledCommands.includes(command.name)) {
           if (command.userPermissions && command.userPermissions.every(p => message.member.hasPermission(p)) && !all)
@@ -82,10 +73,8 @@ module.exports = class HelpCommand extends Command {
           }
         }
       });
-
       const total = Object.values(commands).reduce((a, b) => a + b.length, 0) - commands[OWNER].length;
       const size = message.client.commands.size - commands[OWNER].length;
-
       embed // Build help embed
         .setTitle('Weewoo\'s Commands')
         .setDescription(stripIndent`
@@ -94,33 +83,27 @@ module.exports = class HelpCommand extends Command {
           ${(!all && size != total) ? `**All Commands:** \`${prefix}help all\`` : ''}
         `)
         .setColor(message.guild.me.displayHexColor);
-
       for (const type of Object.values(message.client.types)) {
         if (type === OWNER && !message.client.isOwner(message.member)) continue;
         if (commands[type][0])
           embed.addField(`**${emojiMap[type]} [${commands[type].length}]**`, commands[type].join(' '));
       }
-
       embed.addField(
         '**Links**', 
         '_ _'
       );
-
       let button = new disbut.MessageButton()
       .setStyle('url') //default: blurple
       .setLabel('Invite me') //default: NO_LABEL_PROVIDED
       .setURL('https://discord.com/api/oauth2/authorize?client_id=819584400035020860&permissions=8&scope=bot')
-  
       let button2 = new disbut.MessageButton()
       .setStyle('url')
       .setLabel('Support server')
       .setURL('https://discord.gg/ekMQH384KC')
-  
       let button3 = new disbut.MessageButton()
       .setStyle('url')
       .setLabel('Repository')
       .setURL('https://github.com/Ranger-4297/Weewoo')
-
       message.channel.send({
         embed: embed,
         buttons: [
